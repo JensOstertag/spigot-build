c357cb56bde577a2620974c94e4bcccfaa8bbada
JensOstertag <JensOstertag@gmx.de>
2023-08-26 23:28:05 +0200

    Fix not restored experience and levels

--- src/main/java/de/korzhorz/build/util/InventoryData.java
+++ src/main/java/de/korzhorz/build/util/InventoryData.java
@@ -9,17 +9,23 @@ public class InventoryData {
     private final ItemStack[] inventory;
     private final ItemStack[] armor;
     private final GameMode gameMode;
+    public final int level;
+    public final float exp;
     
     public InventoryData(Player player) {
         this.player = player;
         this.inventory = player.getInventory().getContents();
         this.armor = player.getInventory().getArmorContents();
         this.gameMode = player.getGameMode();
+        this.level = player.getLevel();
+        this.exp = player.getExp();
     }
     
     public void restore() {
         this.player.getInventory().setContents(this.inventory);
         this.player.getInventory().setArmorContents(this.armor);
         this.player.setGameMode(this.gameMode);
+        this.player.setLevel(this.level);
+        this.player.setExp(this.exp);
     }
 }
